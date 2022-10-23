503935511d742e2851dc67979713c080998a68d6
oscarfiscal <oscarfiscalcp@gmail.com>
2022-10-23 00:33:34 -0500

    export data from database

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,11 +13,20 @@ use App\Repository\ProductRepository;
 // Include paginator interface
 use Knp\Component\Pager\PaginatorInterface;
 
+//phpOffice
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+
 
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'product')]
-    
+
     public function index(ProductRepository $product, PaginatorInterface $paginator, Request $request): Response
     {
       
@@ -86,5 +95,86 @@ class ProductController extends AbstractController
         $this->addFlash('success', 'Producto eliminado correctamente');
         return $this->redirectToRoute('product');
        
+    }
+
+    #[Route('/excel', name: 'excel')]
+    public function getExcel(ProductRepository $product){
+
+        $listProduct = $product->findAll();
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Codigo');
+        $sheet->setCellValue('B1', 'Nombre del producto');
+        $sheet->setCellValue('C1', 'Descripcion');
+        $sheet->setCellValue('D1', 'Marca');
+        $sheet->setCellValue('E1', 'Precio');
+        $sheet->setCellValue('F1', 'Fecha de creacion');
+        $sheet->setCellValue('G1', 'Categoria');
+       
+
+        $style=[
+            'font' => [
+                'bold' => true,
+                'size' => 12,
+                'color' => ['argb' => 'FFFFFF'],
+                'name' => 'Arial'
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => ['argb' => '000000'],
+            ],
+        ];
+
+       
+        $sheet->getStyle('A1:G1')->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('000000');
+        $sheet->getStyle('A1:G1')->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_WHITE);
+
+        for ($i = 0; $i < count($listProduct); $i++) {
+            $counter = $i + 2;
+            $sheet->setCellValue("A" .  $counter, $listProduct[$i]->getCode());
+            $sheet->getStyle("A" . $counter)->getFill()->setFillType(Fill::FILL_SOLID);
+            $sheet->getStyle("A" . $counter)->getFill()->getStartColor()->setRGB("dc2780");
+
+            $sheet->setCellValue("B" . $counter, $listProduct[$i]->getName());
+                 
+            $sheet->setCellValue("C" . $counter, $listProduct[$i]->getDescription());
+
+            $sheet->setCellValue("D" . $counter, $listProduct[$i]->getBrand());
+
+            $sheet->setCellValue("E" . $counter, $listProduct[$i]->getPrice());
+
+            $sheet->setCellValue("F" . $counter, $listProduct[$i]->getCreatedAt());
+
+            $sheet->setCellValue("G" . $counter, $listProduct[$i]->getCategory());
+
+        }
+
+        $sheet->getStyle('A1:G1')->applyFromArray($style);
+
+        $sheet->setTitle('Productos');
+        //column dimension
+        $sheet->getColumnDimension('A')->setWidth(20);
+        $sheet->getColumnDimension('B')->setWidth(20);
+        $sheet->getColumnDimension('C')->setWidth(20);
+        $sheet->getColumnDimension('D')->setWidth(20);
+        $sheet->getColumnDimension('E')->setWidth(20);
+        $sheet->getColumnDimension('F')->setWidth(20);
+        $sheet->getColumnDimension('G')->setWidth(20);
+
+
+        $writer = new Xlsx($spreadsheet);
+
+        // file name and temporal file
+        $actualDate = (new \DateTime())->format('Y-m-d');
+        $fileName= 'Productos-'.$actualDate.'.xlsx';
+        $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+
+        $writer->save($temp_file);
+
+        return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+
     }
 }
